38f8d34667bd36f3469a8079c84e3211855218da
bibo <bibo19842003@163.com>
2023-11-05 14:44:33 +0800

    Update django to 4.2.7

--- tushu/settings.py
+++ tushu/settings.py
@@ -138,4 +138,8 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 5
 }
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
